93a8b545838c30509cdfc5c55fb1d8422c4c9b79
ryry-shi <ryadboulahrouf84@outlook.fr>
2024-03-08 19:22:28 +0100

    fin du projet

--- README.md
+++ README.md
@@ -1,1 +1,45 @@
-# P12_boulahrouf
+### Description
+- `CRM d'Epic`
+
+### Version
+- `1.00`
+
+### Version de Python
+
+- `3.12`
+
+#### Cloner le repository
+
+- `git clone https://github.com/ryry-shi/P12_boulahrouf.git`
+
+### Aller dans le répertoire
+
+- `cd P12_boulahrouf`
+
+#### Créer l'environnement virtuel
+
+- `python -m venv venv`
+
+#### Activer l'environnement virtuel
+
+- `source venv/scripts/activate`
+
+#### Installer les dépendances
+
+- `pip install --requirement requirements.txt`
+
+#### Lancer l'application
+
+- `python main.py`
+
+#### Linting
+
+- `flake8`
+
+#### Coverage
+
+- `coverage report`
+
+#### Tests unitaires
+
+- `pytest`
--- menu_factory.py
+++ menu_factory.py
@@ -7,13 +7,13 @@ def create_menu_factory(name):
             return lambda connect, register: Menu(
                 "Menu principal", (
                     ("Se connecter", connect),
-                    ("S'enregistrer", register),
+                    ("Créer un compte gestionnaire", register),
                     ("Quitter", print)
                 )
             )
-        case "GESTIONNAIRE":
-            return lambda read_only, create_collaborator, update_collaborator, delete_collaborator, create_contract_gestion, update_contract_gestion, update_event: Menu(
-                f"Menu gestionnaire",
+        case "GESTION":
+            return lambda disconnect, read_only, create_collaborator, update_collaborator, delete_collaborator, create_contract_gestion, update_contract_gestion, assign_support_event: Menu(
+                "Menu gestionnaire",
                 (
                     ("Consulter la base de données", read_only),
                     ("Créer un collaborator", create_collaborator),
@@ -21,15 +21,16 @@ def create_menu_factory(name):
                     ("Supprimer un collaborator", delete_collaborator),
                     ("Créer un contrat", create_contract_gestion),
                     ("Modifier un contrat", update_contract_gestion),
-                    ("Update pour associer un collaborateur support à un événement", print),
-                    ("Retour", print)
+                    ("Associer un collaborateur support à un événement", assign_support_event),
+                    ("Se déconnecter", disconnect)
                 )
             )
         case "COMMERCIAL":
-            return lambda read_only, create_client, update_client, delete_client, update_contract, create_event, delete_event: Menu(
-                f"Menu commercial",
+            return lambda disconnect, read_only, create_client, update_client, delete_client, update_contract, create_event, delete_event, read_non_signed_contracts: Menu(
+                "Menu commercial",
                 (
                     ("Consulter la base de données", read_only),
+                    ("Consulter les contrats non signés", read_non_signed_contracts),
                     ("Créer un client", create_client),
                     ("Modifier un client qui m'est attribué", update_client),
                     ("Supprimer un client qui m'est attribué", delete_client),
@@ -37,15 +38,27 @@ def create_menu_factory(name):
                     ("Supprimer un contrat qui m'est attribué", "delete_contract"),
                     ("Créer un événement pour un client qui a signé un contrat", create_event),
                     ("Supprimer un event", delete_event),
-                    ("Retour", print)
+                    ("Se déconnecter", disconnect)
                 )
             )
         case "SUPPORT":
-            return lambda read_only, update_event: Menu(
-                f"Menu support",
+            return lambda disconnect, read_only, update_event, read_support_events: Menu(
+                "Menu support",
                 (                
                     ("Consulter la base de données", read_only),
-                    ("Mettre à jour un évènement qui m'est attribué", print),
-                    ("Retour", print)
+                    ("Consulter les évènements qui me sont attribués", read_support_events),
+                    ("Mettre à jour un évènement qui m'est attribué", update_event),
+                    ("Se déconnecter", disconnect)
                 )
             )
+        case "CONSULTER":
+            return lambda read_clients, read_contracts, read_events: Menu(
+            'Menu consultation',
+            (               
+                ("Consulter les clients", read_clients),
+                ("Consulter les Contracts", read_contracts),
+                ("Consulter les évenements", read_events),
+                ("Retour", print)
+            )
+        )
+            
